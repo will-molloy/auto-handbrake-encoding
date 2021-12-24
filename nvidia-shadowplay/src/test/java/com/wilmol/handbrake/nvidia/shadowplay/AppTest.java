b8bb168bdf089cfd0f09ab91adb8a230ee1eebda
Will Molloy <willjoemolloy@gmail.com>
2021-12-24 16:00:40 +1300

    Add component tests
    2 simple scenarios, 1 where original videos are kept and another where
    they are deleted.

--- nvidia-shadowplay/src/test/java/com/wilmol/handbrake/nvidia/shadowplay/AppTest.java
+++ nvidia-shadowplay/src/test/java/com/wilmol/handbrake/nvidia/shadowplay/AppTest.java
@@ -2,30 +2,92 @@ package com.wilmol.handbrake.nvidia.shadowplay;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import com.google.common.io.Resources;
 import com.wilmol.handbrake.core.Cli;
 import com.wilmol.handbrake.core.HandBrake;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 /**
- * AppTest.
+ * Component test.
  *
  * @author <a href=https://wilmol.com>Will Molloy</a>
  */
+@SuppressFBWarnings("UWF_FIELD_NOT_INITIALIZED_IN_CONSTRUCTOR")
 @ExtendWith(MockitoExtension.class)
 class AppTest {
 
-  @Mock private HandBrake handBrake;
+  private Path testDirectory;
+  private Path testVideo;
 
-  @Mock private Cli cli;
+  private App app;
 
-  @InjectMocks private App app;
+  @BeforeEach
+  void setUp() throws Exception {
+    testDirectory = Path.of("AppTest");
+    testVideo = Path.of(Resources.getResource("test-video.mp4").toURI());
+
+    Cli cli = new Cli(ProcessBuilder::new);
+    HandBrake handBrake = new HandBrake(cli);
+    app = new App(handBrake, cli);
+  }
+
+  @AfterEach
+  void tearDown() throws IOException {
+    FileUtils.deleteDirectory(testDirectory.toFile());
+  }
 
   @Test
-  void test() {
-    assertThat(app).isNotNull();
+  void notDeletingOriginalVideos_encodesVideoFilesAndKeepsOriginals() throws Exception {
+    // setup data
+    Files.createDirectories(testDirectory.resolve(Path.of("KeepOriginals/NestedFolder")));
+    Files.copy(testVideo, testDirectory.resolve(Path.of("KeepOriginals/video1.mp4")));
+    Files.copy(testVideo, testDirectory.resolve(Path.of("KeepOriginals/video2.mp4")));
+    Files.copy(testVideo, testDirectory.resolve(Path.of("KeepOriginals/NestedFolder/video3.mp4")));
+
+    Path videosPath = Path.of("AppTest/KeepOriginals");
+    app.run(videosPath, false, false);
+
+    // originals still exist
+    assertThat(Files.exists(Path.of("AppTest/KeepOriginals/video1.mp4"))).isTrue();
+    assertThat(Files.exists(Path.of("AppTest/KeepOriginals/video2.mp4"))).isTrue();
+    assertThat(Files.exists(Path.of("AppTest/KeepOriginals/NestedFolder/video3.mp4"))).isTrue();
+
+    // encoded videos exist
+    assertThat(Files.exists(Path.of("AppTest/KeepOriginals/video1 - CFR 60 FPS.mp4"))).isTrue();
+    assertThat(Files.exists(Path.of("AppTest/KeepOriginals/video2 - CFR 60 FPS.mp4"))).isTrue();
+    assertThat(Files.exists(Path.of("AppTest/KeepOriginals/NestedFolder/video3 - CFR 60 FPS.mp4")))
+        .isTrue();
+  }
+
+  @Test
+  void deletingOriginalVideos_encodesVideoFilesAndDeletesOriginals() throws Exception {
+    // setup data
+    Files.createDirectories(testDirectory.resolve(Path.of("DeleteOriginals/NestedFolder")));
+    Files.copy(testVideo, testDirectory.resolve(Path.of("DeleteOriginals/video1.mp4")));
+    Files.copy(testVideo, testDirectory.resolve(Path.of("DeleteOriginals/video2.mp4")));
+    Files.copy(testVideo, testDirectory.resolve(Path.of("DeleteOriginals/NestedFolder/video3.mp4")));
+
+    Path videosPath = Path.of("AppTest/DeleteOriginals");
+    app.run(videosPath, true, false);
+
+    // originals deleted
+    assertThat(Files.exists(Path.of("AppTest/DeleteOriginals/video1.mp4"))).isFalse();
+    assertThat(Files.exists(Path.of("AppTest/DeleteOriginals/video2.mp4"))).isFalse();
+    assertThat(Files.exists(Path.of("AppTest/DeleteOriginals/NestedFolder/video3.mp4"))).isFalse();
+
+    // encoded videos exist
+    assertThat(Files.exists(Path.of("AppTest/DeleteOriginals/video1 - CFR 60 FPS.mp4"))).isTrue();
+    assertThat(Files.exists(Path.of("AppTest/DeleteOriginals/video2 - CFR 60 FPS.mp4"))).isTrue();
+    assertThat(Files.exists(Path.of("AppTest/DeleteOriginals/NestedFolder/video3 - CFR 60 FPS.mp4")))
+            .isTrue();
   }
 }
