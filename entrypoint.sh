e1fcec5cfd56b8d0222b09b95df88a46a2493557
Will Molloy <willjoemolloy@gmail.com>
2022-07-11 21:17:14 +1200

    Sleep when run fails
    So you can view the logs, see what went wrong.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,11 @@
 # hacky but it works (for my use case)
 if [ -z "$(ls -A /archive)" ]; then
   echo "/archive directory empty, network drive not mounted?"
+  sleep infinity
   exit 1
 fi
 
-
-./gradlew :nvidia-shadowplay:run -PinputDirectory="/input" -PoutputDirectory="/output" -ParchiveDirectory="/archive"
+if not ./gradlew :nvidia-shadowplay:run -PinputDirectory="/input" -PoutputDirectory="/output" -ParchiveDirectory="/archive"; then
+  sleep infinity
+  exit 1
+fi
