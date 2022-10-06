912b960ba5e0a55f5348fc3d1db1d4af1f67a031
Will Molloy <willjoemolloy@gmail.com>
2022-10-06 16:50:06 +1300

    Remove sleep from Dockerfile entrypoint
    non-zero exit code is sufficient to write a bash script that does the
    sleep for you.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,10 @@
 # hacky but it works (for my use case)
 if [ -z "$(ls -A /archive)" ]; then
   echo "/archive directory empty, network drive not mounted?"
-  sleep infinity
   exit 1
 fi
 
 if ! ./gradlew :nvidia-shadowplay:run -PinputDirectory="/input" -PoutputDirectory="/output" -ParchiveDirectory="/archive"; then
-  sleep infinity
   exit 1
 fi
 
