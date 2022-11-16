e57dfa19646676e67b4adcd2ba8a248100940842
Will Molloy <willjoemolloy@gmail.com>
2022-11-16 17:23:38 +1300

    Remove CloseableExectutorService (built into jdk19)

--- auto-handbrake-cfr/src/main/java/com/willmolloy/handbrake/cfr/util/Async.java
+++ auto-handbrake-cfr/src/main/java/com/willmolloy/handbrake/cfr/util/Async.java
@@ -1,12 +1,8 @@
 package com.willmolloy.handbrake.cfr.util;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
-import com.google.common.util.concurrent.ForwardingExecutorService;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
 /**
@@ -23,48 +19,10 @@ public final class Async {
     // (want to do this so the logs are easier to follow, and don't want to set up structured
     // logging)
     // could use 'new Thread' (with FutureTask), but then you don't get the CompletableFuture API
-    try (CloseableExecutorService executor =
-        new CloseableExecutorService(
-            Executors.newSingleThreadExecutor(x -> new Thread(x, threadName)))) {
+    // TODO newVirtualThreadPerTaskExecutor
+    try (ExecutorService executor =
+        Executors.newSingleThreadExecutor(r -> new Thread(r, threadName))) {
       return CompletableFuture.supplyAsync(supplier, executor);
-    }
-  }
-
-  // TODO remove when we upgrade to JDK 19
-  private static class CloseableExecutorService extends ForwardingExecutorService
-      implements AutoCloseable {
-    private final ExecutorService delegate;
-
-    CloseableExecutorService(ExecutorService delegate) {
-      this.delegate = checkNotNull(delegate);
-    }
-
-    @Override
-    protected ExecutorService delegate() {
-      return delegate;
-    }
-
-    @Override
-    public void close() {
-      // copy paste from JDK 19 EA
-      boolean terminated = isTerminated();
-      if (!terminated) {
-        shutdown();
-        boolean interrupted = false;
-        while (!terminated) {
-          try {
-            terminated = awaitTermination(1L, TimeUnit.DAYS);
-          } catch (InterruptedException e) {
-            if (!interrupted) {
-              shutdownNow();
-              interrupted = true;
-            }
-          }
-        }
-        if (interrupted) {
-          Thread.currentThread().interrupt();
-        }
-      }
     }
   }
 
